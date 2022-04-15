37da6734786c2078f9a17f9620a000c98ba50440
SchmouetSchmouet <antoine.hugi@gmail.com>
2022-04-15 15:24:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+const Header = (props) => {
+  return(
+  <div>
+  <h1>
+  {props.title}
+  </h1>
+  </div>
+  )
+  }
+  const Content = (props) => {
+  return(
+  <div>
+  <p>
+  {props.title} {props.exercise} 
+  </p>
+  </div>
+  )
+  }
+  const Total = (props) => {
+  return(
+  <div>
+  <p>
+  Number of exercises {props.one+props.two+props.three}
+  </p>
+  </div>
+  )
+  } 
+  const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
+  return (
+  <div>
+  <Header title={course}/>
+  <Content title={part1} exercise={exercises1}/>
+  <Content title={part2} exercise={exercises2}/>
+  <Content title={part3} exercise={exercises3}/>
+  <Total one={exercises1} two={exercises2} three={exercises3}/>
+  
+  </div>
+  )
+  }
+  
+  export default App
+  
